36a73fc19bb78d32a9f6f1d9a29854b1c873451c
covagashi <covaga94@gmail.com>
2024-07-29 19:17:08 +0200

    Changes to be committed:

---
+++ src/EPLAN Scripting Project/02_Actions_ToRun/04_Action_overloaded.cs
@@ -0,0 +1,17 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Scripting;
+using System.Windows.Forms;
+
+public class Class
+{
+  [DeclareAction("XSDShowMatchingMasterDialogAction", 50)]
+  public void Function()
+  {
+    MessageBox.Show("¡Precaución!");
+
+    ActionCallingContext acc = new ActionCallingContext();
+    Eplan.EplApi.ApplicationFramework.Action action =
+        new ActionManager().FindBaseActionFromFunctionAction(false);    
+    action.Execute(acc);
+  }
+}
